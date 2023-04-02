1db89131c0ca3d4638192be9ac4ef176f6670b5c
Jeiel Tarazona Lovera <jeiel.disas@gmail.com>
2023-04-01 22:58:55 -0500

    add assets resources to pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:fit_healthy/domain/utils/themes/color_theme.dart';
 import 'package:fit_healthy/ui/login/login_page.dart';
 import 'package:fit_healthy/ui/main/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
+  initializeDateFormatting("es_ES");
   runApp(const MyApp());
 }
 
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme(
               primary: Palette.green.shade200,
@@ -31,8 +34,8 @@ class MyApp extends StatelessWidget {
               primaryVariant: Palette.green.shade50,
               secondaryVariant: Palette.green.shade50)),
       /*home: const MyHomePage(title: 'fit and Health'),*/
-      //home: const LogInPage(),
-      home: const MainPage(),
+      home: const LogInPage(),
+      //home: const MainPage(),
     );
   }
 }
